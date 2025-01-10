e6ab50f7a02c657a5295c240b5e9358c1409dc23
“Thomas <tjwhitephd@gmail.com>
2025-01-09 17:44:58 -0800

    updated bootstrap form on contacts page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -45,6 +45,39 @@ export default function Contact() {
         />
         <button type="submit">Submit</button>
       </form>
+
+      <form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            name="visitorEmail"
+            value={visitorEmail}
+            onChange={handleInputChange}
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+
+          <div className="form-floating">
+            <textarea
+              className="form-control"
+              placeholder="Leave a comment here"
+              id="floatingTextarea2"
+              // {*todo: consider React styling to set height at 100px or so}
+            ></textarea>
+            <label htmlFor="floatingTextarea2">Comments</label>
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
