d9cac5531e82745197f514627db61cc9b2981081
“Thomas <tjwhitephd@gmail.com>
2025-01-14 18:42:08 -0800

    updated contact form

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -4,6 +4,7 @@ export default function Contact() {
   const [visitorName, setVisitorName] = useState("");
   const [visitorEmail, setVisitorEmail] = useState("");
   const [visitorMessage, setVisitorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     if (name === "visitorName") {
@@ -15,38 +16,46 @@ export default function Contact() {
     }
   };
 
+  function validateEmail(email) {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    console.log(visitorName, visitorEmail, visitorMessage);
+    if (!validateEmail(email)) {
+      setErrorMessage("Email is invalid");
+      return;
+    }
+    // TODO: error tells them to enter a name if they stray (!checkVisitorName(visitorName)) {
+    //   setErrorMessage(
+    //     `Don't leave without leaving your name!`
+    //   );
+    //   return;
+    // }
   };
 
   return (
     <div>
       <h1>Contact Page</h1>
-      <p>{visitorName} </p>
+      <div id="emailHelp" className="form-text">
+        We'll never share your information with anyone else.
+      </div>
       <form onSubmit={handleFormSubmit}>
-        <input
-          type="text"
-          name="visitorName"
-          value={visitorName}
-          onChange={handleInputChange}
-        />
-        <input
-          type="email"
-          name="visitorEmail"
-          value={visitorEmail}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="visitorMessage"
-          value={visitorMessage}
-          onChange={handleInputChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
+        <div className="mb-3">
+          <label htmlFor="Your Name" className="form-label">
+            Your Name
+          </label>
+          <input
+            type="text"
+            name="visitorName"
+            value={visitorName}
+            onChange={handleInputChange}
+            className="form-control"
+          />
+        </div>
 
-      <form>
         <div className="mb-3">
           <label htmlFor="exampleInputEmail1" className="form-label">
             Email address
@@ -59,19 +68,19 @@ export default function Contact() {
             className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
-          />
-          <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
-          </div>
-
+          />{" "}
+          <label htmlFor="floatingTextarea2">Comments</label>
           <div className="form-floating">
             <textarea
+              type="text"
+              name="visitorMessage"
+              value={visitorMessage}
+              onChange={handleInputChange}
               className="form-control"
               placeholder="Leave a comment here"
               id="floatingTextarea2"
               // {*todo: consider React styling to set height at 100px or so}
             ></textarea>
-            <label htmlFor="floatingTextarea2">Comments</label>
           </div>
         </div>
         <button type="submit" className="btn btn-primary">
